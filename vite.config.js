58bfd3aa89058cf8aae1d7fd2e86e2a8a898f474
Faruque23 <faruquehossain2011@gmail.com>
2026-04-14 22:07:52 +0600

    Final Commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: '/B13-Assignment-06-main/',
   plugins: [react()],
 })
